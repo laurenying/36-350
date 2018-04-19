09e7ed8c3b6121ffe54fbda887bf8c40a5bd6da6
laurenying <yuhangy@andrew.cmu.edu>
2018-04-19 15:57:21 -0400

    change 0.5 to cutoff

--- hello_world.R
+++ hello_world.R
@@ -6,3 +6,15 @@ generate_data=function(n,p){
   return (list(covariates,responses))
 }
  
+
+model_select=function(covariates, responses,cutoff){
+  res=lm(responses ~ covariates)
+  res.sum=summary(res)
+  p.val=res.sum$coefficients[2:(ncol(covariates)+1),"Pr(>|t|)"]
+  ind=which(p.val<=cutoff)
+  new=lm(responses~covariates[,ind])
+  new.sum=summary(new)
+  new.pval=new.sum$coefficients[,"Pr(>|t|)"]
+  return(new.pval)
+  
+}
